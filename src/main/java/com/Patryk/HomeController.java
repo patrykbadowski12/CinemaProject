99d10f12b5a4bba41ebbf4294169ae13e5b017fc
Badowski Patryk <patryk_badowski@o2.pl>
2018-05-16 15:40:06 +0200

    add a Time unreservation and remove Film

--- src/main/java/com/Patryk/HomeController.java
+++ src/main/java/com/Patryk/HomeController.java
@@ -1,7 +1,9 @@
 package com.Patryk;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
@@ -16,8 +18,8 @@ public class HomeController {
 
 	SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
 
-	public List<Films> listOfFilms = new ArrayList<Films>();
-	
+	private List<Films> listOfFilms = new ArrayList<Films>();
+
 	Films film;
 
 	public List<Films> getListOfFilms() {
@@ -37,7 +39,7 @@ public class HomeController {
 	public void setSit(int sit) {
 		this.sit = sit;
 	}
-	
+
 	int numberOfFilm;
 
 	public int getNumberOfFilm() {
@@ -50,97 +52,149 @@ public class HomeController {
 
 	@RequestMapping("/")
 	public String index() {
-		
-		listOfFilms.add(new Films(1, "Avengers","07:42:00", "04.08.2015",
-				"http://1.fwcdn.pl/po/37/58/693758/7839647.3.jpg"));
-		listOfFilms.add(new Films(2, "Spider Man", "09:00:00", "06.10.2018",
+
+		listOfFilms.add(
+				new Films(1, "Avengers", "07:42:00", "04.08.2015", "http://1.fwcdn.pl/po/37/58/693758/7839647.3.jpg"));
+		listOfFilms.add(new Films(2, "Spider Man", "15:23:00", "16.5.2018",
 				"http://moviesroom.pl/images/0.Aktualizacja_listopad/Pat/spider-man-homecoming-poster-plakat-1000x600.jpg"));
-		listOfFilms.add(new Films(3, "Thor", "13:30:00", "06.10.2018",
+		listOfFilms.add(new Films(3, "Thor", "14:50:00", "06.10.2018",
 				"https://images-na.ssl-images-amazon.com/images/G/01/digital/video/hero/Movies/2013/ThorDarkWorld_2194942100-TDW0NNG1._V362444527_RI_SX940_.jpg"));
 
 		return "redirect:/index";
 	}
 
-	@RequestMapping(value="/index", method = RequestMethod.GET)
+	@RequestMapping(value = "/index", method = RequestMethod.GET)
 	public String getMethod(Model model) {
-		
+
 		model.addAttribute("films", listOfFilms);
 		return "index";
 	}
-	
-	@RequestMapping(value="/reservation", method = RequestMethod.GET)
-	public String reservation (Model model) {
-		
-		Persons person= new Persons();
-		
-		model.addAttribute("sit",sit);
-		model.addAttribute("film",film);
+
+	@RequestMapping(value = "/reservation", method = RequestMethod.GET)
+	public String reservation(Model model) {
+
+		Persons person = new Persons();
+
+		model.addAttribute("sit", sit);
+		model.addAttribute("film", film);
 		model.addAttribute("person", person);
 		return "reservation";
 	}
 
 	@RequestMapping(value = "/reserv", method = RequestMethod.POST)
 	public String signupPost(@ModelAttribute("person") Persons person, @ModelAttribute("sit") int sit, Model model) {
-		
-		if(film.getMapOfPersons().putIfAbsent(sit, person) != null) {
+
+		if (film.getMapOfPersons().putIfAbsent(sit, person) != null) {
 			return "redirect:/reservation";
-		}else{
+		} else {
 			model.addAttribute("films", listOfFilms);
-			return "redirect:/index";
+			return "index";
 		}
 	}
-	
-	@RequestMapping(value="reservationPlace", method = RequestMethod.POST)
-	public String reservationPlace(Model model, @ModelAttribute("numberOfFilm") int numberOfFilm ) {
-		
-		film = listOfFilms.get(numberOfFilm);
-		System.out.println(film);
-		
-		return "redirect:/reservation";
+
+	@RequestMapping(value = "reservationPlace", method = RequestMethod.POST)
+	public String reservationPlace(Model model, @ModelAttribute("numberOfFilm") int numberOfFilm) {
+
+		Date d1 = new Date();
+		System.out.println(d1);
+		try {
+			Date d2 = dateFormat
+					.parse(listOfFilms.get(numberOfFilm).getDate() + " " + listOfFilms.get(numberOfFilm).getTime());
+			long diff = d2.getTime() - d1.getTime();
+			System.out.println(diff);
+			long diffMinutes = diff / (60 * 1000);
+			System.out.println(diffMinutes);
+
+			if (diffMinutes > 0) {
+
+				film = listOfFilms.get(numberOfFilm);
+				System.out.println(film);
+				return "redirect:/reservation";
+			} else {
+				return "redirect:/index";
+			}
+
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		return "redirect:/index";
 	}
 
-
-	@GetMapping("/addFilm")
+	@RequestMapping(value="/addFilm",method = RequestMethod.GET)
 	public String addFilm(Model model) {
 
-	model.addAttribute("film", new Films());
+		model.addAttribute("film", new Films());
 		model.addAttribute("listOfFilms", listOfFilms);
-		
+
 		return "addFilm";
 	}
 
 	@RequestMapping(value = "/addFilm", method = RequestMethod.POST)
 	public String addFilm(@ModelAttribute("film") Films film) {
-		
 
 		listOfFilms.add(film);
-		
-		return "redirect:/index";
-	}
-	@RequestMapping(value="/unreservation", method = RequestMethod.GET)
-	public String unreservation(Model model) {
-		
-		Persons person = new Persons();
-		model.addAttribute("person",person);
-		model.addAttribute("sit",sit);
-		model.addAttribute("numberOfFilm",numberOfFilm);
-		return "unreservation";
-	}
-	
-	@RequestMapping(value="unreserv",  method = RequestMethod.POST)
-	public String unreserv(Model model, @ModelAttribute("sit") int sit, @ModelAttribute("numberOfFilm") int numberOfFilm,
-			@ModelAttribute("person") Persons person) {
-		
-		Persons tempPerson=listOfFilms.get(numberOfFilm).getMapOfPersons().get(sit);
-	
-		if((tempPerson!= null) && (tempPerson.getKey().equals(person.getKey()))) {
-		listOfFilms.get(numberOfFilm).getMapOfPersons().replace(sit, null);
-		System.out.println("udało się zwolnic miejsce");
-		} else {
-			System.out.println("Miejsce jest wolne");
-		}
-		
+
 		return "redirect:/index";
 	}
 
+	@RequestMapping(value = "/removeFilm", method = RequestMethod.GET)
+	public String remove(Model model) {
+		model.addAttribute("listOfFilms", listOfFilms);
+		return "removeFilm";
+	}
+
+	@RequestMapping(value = "/removeFilm", method = RequestMethod.POST)
+	public String removeFilm(Model model, @ModelAttribute("numberOfFilm") int numberOfFilm) {
+
+		getListOfFilms().remove(numberOfFilm);
+		return "redirect:/index";
+	}
+
+	@RequestMapping(value = "/unreservation", method = RequestMethod.GET)
+	public String unreservation(Model model) {
+
+		Persons person = new Persons();
+		model.addAttribute("person", person);
+		model.addAttribute("sit", sit);
+		model.addAttribute("numberOfFilm", numberOfFilm);
+		return "unreservation";
+	}
+
+	@RequestMapping(value = "unreserv", method = RequestMethod.POST)
+	public String unreserv(Model model, @ModelAttribute("sit") int sit,
+			@ModelAttribute("numberOfFilm") int numberOfFilm, @ModelAttribute("person") Persons person) {
+
+		Date d1 = new Date();
+		System.out.println(d1);
+		try {
+			Date d2 = dateFormat
+					.parse(listOfFilms.get(numberOfFilm).getDate() + " " + listOfFilms.get(numberOfFilm).getTime());
+			long diff = d2.getTime() - d1.getTime();
+			System.out.println(diff);
+			long diffMinutes = diff / (60 * 1000);
+			System.out.println(diffMinutes);
+
+			if (diffMinutes >= 14) {
+
+				Persons tempPerson = listOfFilms.get(numberOfFilm).getMapOfPersons().get(sit);
+
+				if ((tempPerson != null) && (tempPerson.getKey().equals(person.getKey()))) {
+					listOfFilms.get(numberOfFilm).getMapOfPersons().replace(sit, null);
+					System.out.println("udało się zwolnic miejsce");
+					return "redirect:/index";
+				} else {
+					System.out.println("Miejsce jest wolne");
+					return "redirect:/unreservation";
+				}
+			} else {
+				System.out.println("za pozno");
+				return "redirect:/index";
+			}
+
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+
+		return "redirect:/index";
+	}
 }
